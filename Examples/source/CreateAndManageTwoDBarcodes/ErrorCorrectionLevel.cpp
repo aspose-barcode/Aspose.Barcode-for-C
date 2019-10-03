c18e0698b1c6cdb18dfb61a90360a8c7d1519976
muzammil-khan <muzammil.khan@aspose.com>
2019-10-03 13:11:59 +0500

    Added ErrorCorrectionLevel

--- Examples/source/CreateAndManageTwoDBarcodes/ErrorCorrectionLevel.cpp
+++ Examples/source/CreateAndManageTwoDBarcodes/ErrorCorrectionLevel.cpp
@@ -5,24 +5,23 @@ If you do not wish to use NuGet, you can manually download Aspose.BarCode for .N
 install it and then add its reference to this project. For any issues, questions or suggestions
 please feel free to contact us using http://www.aspose.com/community/forums/default.aspx
 */
-#include "ManagePDF417Barcode.h"
+#include "ErrorCorrectionLevel.h"
 
 #include <system/string.h>
 #include <system/shared_ptr.h>
 #include <system/object.h>
 #include <system/environment.h>
 #include <system/console.h>
-#include <Generation/Pdf417ErrorLevel.h>
-#include <Generation/Pdf417CompactionMode.h>
 #include <Generation/EncodeTypes/SymbologyEncodeType.h>
 #include <Generation/EncodeTypes/EncodeTypes.h>
-#include <BarCode.Generation/BarcodeGenerator.h>
 #include <Generation/BarCodeImageFormat.h>
+#include <BarCode.Generation/BarcodeGenerator.h>
+#include <cstdint>
 #include <BarCode.Generation/GenerationParameters/BaseGenerationParameters.h>
 #include <BarCode.Generation/GenerationParameters/BarcodeParameters.h>
+#include <Generation/Pdf417ErrorLevel.h>
 
 #include "RunExamples.h"
-
 
 using namespace Aspose::BarCode::Generation;
 namespace Aspose {
@@ -35,25 +34,23 @@ namespace Aspose {
 
 				namespace CreateAndManageTwoDBarcodes {
 
-					RTTI_INFO_IMPL_HASH(2695409973u, ::Aspose::BarCode::Examples::CSharp::CreateAndManageTwoDBarcodes::ManagePDF417Barcode, ThisTypeBaseTypesInfo);
+					RTTI_INFO_IMPL_HASH(2991363062u, ::Aspose::BarCode::Examples::CSharp::CreateAndManageTwoDBarcodes::ErrorCorrectionLevel, ThisTypeBaseTypesInfo);
 
-					void ManagePDF417Barcode::Run()
+					void ErrorCorrectionLevel::Run()
 					{
-						//ExStart:ManagePDF417Barcode
+						//ExStart:ErrorCorrectionLevel
 						// The path to the documents directory.
 						System::String dataDir = RunExamples::GetDataDir_CreateAndManage2DBarCodes();
 
-						// Instantiate barcode object
-						System::SharedPtr<BarcodeGenerator> generator = System::MakeObject<BarcodeGenerator>(EncodeTypes::Pdf417);
+						// Instantiate barcode object and set Barcode CodeText & Symbology
+						System::SharedPtr<BarcodeGenerator> generator = System::MakeObject<BarcodeGenerator>(EncodeTypes::Pdf417, u"1234567890"); 
+						
+						generator->get_Parameters()->get_Barcode()->get_Pdf417()->set_Pdf417ErrorLevel(Pdf417ErrorLevel::Level8);
 
-						// Set Pdf417 Error correction level, Compaction Mode to Text and CodeText for barcode
-						generator->get_Parameters()->get_Barcode()->get_Pdf417()->set_Pdf417ErrorLevel(Aspose::BarCode::Pdf417ErrorLevel::Level8);
-						generator->get_Parameters()->get_Barcode()->get_Pdf417()->set_Pdf417CompactionMode(Aspose::BarCode::Pdf417CompactionMode::Text);
-						generator->set_CodeText(u"1234567890");
-						generator->Save(dataDir + u"ManagePDF417Barcode_out.bmp", BarCodeImageFormat::Bmp);
+						generator->Save(dataDir + u"ErrorCorrectionLevel_out.bmp", BarCodeImageFormat::Bmp);
 
-						System::Console::WriteLine(System::Environment::get_NewLine() + u"Barcode saved at " + dataDir + u"ManagePDF417Barcode_out.bmp");
-						//ExEnd:ManagePDF417Barcode
+						System::Console::WriteLine(System::Environment::get_NewLine() + u"Barcode saved at " + dataDir + u"ErrorCorrectionLevel_out.bmp");
+						//ExEnd:ErrorCorrectionLevel
 					}
 
 				} // namespace CreateAndManageTwoDBarcodes
